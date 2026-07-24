2a9bf5a4c07dd286532b6265efa115470055f011
Colondry <sienta052@gmail.com>
2026-07-24 20:26:58 +0700

    Enhance lexer with additional token types

--- lexer.hpp
+++ lexer.hpp
@@ -6,17 +6,27 @@
 
 enum class TokenType {
     // literals / names
-    Identifier, Number, String,
+    Identifier, Number, String, Char,
     // keywords
-    Fn, Ret, If, While, For, In, Import, List, Print, PrintLine, Input,
+    Fn, Ret, 
+    If, Elif, Else, 
+    While, For, 
+    In, 
+    Import, 
+    List, 
+    Print, PrintLine, Input, InputString,
+    Continue, Break,
+    Clear,
     // built-in types
     TypeInt, TypeString, TypeFloat, TypeDouble, TypeByte, TypeChar, TypeBool, TypeVoid,
     // punctuation
     LParen, RParen, LBrace, RBrace, LBracket, RBracket,
-    Comma, Semicolon, Colon, DoubleColon, At,
+    Comma, Semicolon, Colon, DoubleColon, At, Dot,
     // operators
     Assign, Eq, Neq, Lt, Gt, Le, Ge, AndAnd, OrOr,
-    Plus, Minus, Star, Slash, Shl, Shr, PlusPlus,
+    Plus, Minus, Star, Slash, Shl, Shr, 
+    PlusPlus, MinusMinus, 
+    AssignAdd, AssignMinus, AssignMulti, AssignSlash,
     // unrecognized character
     Invalid,
     EndOfFile
